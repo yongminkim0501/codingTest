dc5c8b00b39c769208aeeae1808571fea53e781d
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2024-11-20 10:22:43 +0900

    [Silver IV] Title: 스택 2, Time: 776 ms, Memory: 102856 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+point = 0
+result = []
+for _ in range(n):
+  command = sys.stdin.readline().split()
+  if command[0] == '1':
+    stack.append(command[1])
+  elif command[0] == '2':
+    if stack:
+      print(stack.pop())
+    else:
+      print(-1)
+  elif command[0] == '3':
+    print(len(stack))
+  elif command[0] == '4':
+    if stack:
+      print(0)
+    else:
+      print(1)
+  elif command[0] == '5':
+    if stack:
+      print(stack[-1])
+    else:
+      print(-1)
