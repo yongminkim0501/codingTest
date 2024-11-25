279c0536836ed5c7730618a796a4db55596e6dd5
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2024-11-25 16:27:52 +0900

    Update 분산처리.py

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,18 +1,15 @@
 n = int(input())
 for _ in range(n):
     a, b = map(int, input().split())
-    if a % 10 == 0:
-        print(10)
-        continue
-        
-    b = b % 4
-    if b == 0:
-        b = 4
+    tmp = 1
+    a %= 10
     
-    tmp = a % 10
-    for _ in range(b-1):
-        tmp = (tmp * a) % 10
-        
+    while b > 0:
+        if b % 2 == 1:
+            tmp = (tmp * a) % 10
+        a = (a * a) % 10
+        b //= 2
+            
     if tmp == 0:
         print(10)
     else:
