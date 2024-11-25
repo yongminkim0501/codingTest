23014f6fb2fce23b7083279b625102b1f593a947
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2024-11-25 12:40:55 +0900

    Update 쉬운 계단 수.py
    이게 실제 내가 구현한 코드

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,13 +1,22 @@
 n = int(input())
-dp = [[0 for i in range(10)] for j in range(101)]
-for i in range(1, 10):
-    dp[1][i] = 1
-for i in range(2, n + 1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i - 1][1]
-        elif j == 9:
-            dp[i][j] = dp[i - 1][8]
-        else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-print(sum(dp[n]) % 1000000000)
+MOD = 1000000000
+
+if n == 1:
+    print(9)
+else:
+    dp = [1]*10
+    dp[0] = 0
+    
+    for i in range(1, n):
+        next_dp = [0]*10
+        for j in range(10):
+            if j == 0:
+                next_dp[j] = dp[1]
+            elif j == 9:
+                next_dp[j] = dp[8]
+            else:
+                next_dp[j] = dp[j-1] + dp[j+1]
+            next_dp[j] %= MOD
+        dp = next_dp
+            
+    print(sum(dp) % MOD)
