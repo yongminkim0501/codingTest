bff0a0bc730dca4a4bb45f8fa7dfbb4dbf118793
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2025-01-22 15:51:59 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,14 @@
+'''
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+
+첫째 줄에 코드 1의 수행 횟수
+둘째 줄에 코드 1의 수행 횟수를 다항식으로 나타내었을 때, 최고 차항의 차수 
+'''
+n = int(input())
+print(n)
+print(1)
