0e7fbc51c9c3169a5563da5a828692b316a6ed26
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2026-07-30 19:50:43 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,22 @@
+def solution(clothes):
+    answer = 0
+    dic = {}
+    for item in clothes:
+        key = item[1]
+        if key in dic :
+            dic[key] += 1
+        else:
+            dic[key] = 1
+            
+    dot_answer = 1
+    
+    if len(list(dic.values())) == 1:
+        answer += list(dic.values())[0]
+        
+    else:
+        for count in dic.values():
+            dot_answer *= (count+1)
+            
+        answer += (dot_answer-1)
+    
+    return answer
