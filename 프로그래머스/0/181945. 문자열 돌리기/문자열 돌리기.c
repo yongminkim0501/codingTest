68173a702dfb3cf9df71d47414891a61fa3212a8
kim yongmin <69386483+yongminkim0501@users.noreply.github.com>
2026-08-03 20:04:43 +0900

    [level 0] Title: 문자열 돌리기, Time: 1.28 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define LEN_INPUT 11
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    
+    for (int i = 0; i<strlen(s1); i++){
+        char a;
+        a = s1[i];
+        printf("%c\n", a);
+    }
+
+    return 0;
+}
